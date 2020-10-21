0d5bbf5cce82e89502c6f34e42be36b0ccf65bca
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-10-21 11:40:35 -0400

    full CRUD routes both models

--- controllers/ingredientsController.js
+++ controllers/ingredientsController.js
@@ -1,36 +1,82 @@
+const { Router } = require("express");
 const express = require("express");
 const router = express.Router();
 
-const db = require("../models")
+const db = require("../models");
 
 router.get("/api/ingredients", (req, res) => {
-    db.Ingredient.find({})
-      .then((foundIngredients) => {
-        res.json(foundIngredients);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: true,
-          data: null,
-          message: "Failed to retrieve ingredients.",
-        });
+  db.Ingredient.find({})
+    .then((foundIngredients) => {
+      res.json(foundIngredients);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to retrieve ingredients.",
       });
-  });
-  
-  router.post("/api/ingredients", (req, res) => {
-    db.Ingredient.create(req.body)
-      .then((newIngredient) => {
-        res.json(newIngredient);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: true,
-          data: null,
-          message: "Failed to create new ingredient.",
-        });
+    });
+});
+
+router.get("/api/ingredients/:id", (req, res) => {
+  db.Ingredient.findById(req.params.id)
+    .then((foundIngredient) => {
+      res.json(foundIngredient);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: `Failed to retrieve ingredient with id: ${req.params.id}`,
       });
-  });
+    });
+});
+
+router.post("/api/ingredients", (req, res) => {
+  db.Ingredient.create(req.body)
+    .then((newIngredient) => {
+      res.json(newIngredient);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to create new ingredient.",
+      });
+    });
+});
+
+router.put("/api/ingredients/:id", (req, res) => {
+  db.Ingredient.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedIngredient) => {
+      res.json(updatedIngredient);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to update ingredient.",
+      });
+    });
+});
+
+router.delete("/api/ingredients/:id", (req, res) => {
+  db.Ingredient.findByIdAndDelete(req.params.id)
+    .then((deletedItem) => {
+      res.json(deletedItem);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to delete ingredient.",
+      });
+    });
+});
 
 module.exports = router;
--- controllers/pizzaController.js
+++ controllers/pizzaController.js
@@ -19,6 +19,22 @@ router.get("/api/pizzas", (req, res) => {
     });
 });
 
+router.get("/api/pizzas/:id", (req, res) => {
+  db.Pizza.findById(req.params.id)
+    .populate("ingredients")
+    .then((foundPizza) => {
+      res.json(foundPizza);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: `Failed to retrieve pizza with id: ${req.params.id}`,
+      });
+    });
+});
+
 router.post("/api/pizzas", (req, res) => {
   db.Pizza.create(req.body)
     .then((newPizza) => {
@@ -30,6 +46,36 @@ router.post("/api/pizzas", (req, res) => {
         error: true,
         data: null,
         message: "Failed to create new pizza.",
+      });
+    });
+});
+
+router.put("/api/pizzas/:id", (req, res) => {
+  db.Pizza.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedPizza) => {
+      res.json(updatedPizza);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to update pizza.",
+      });
+    });
+});
+
+router.delete("/api/pizzas/:id", (req, res) => {
+  db.Pizza.findByIdAndDelete(req.params.id)
+    .then((deletedPizza) => {
+      res.json(deletedPizza);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to delete pizza.",
       });
     });
 });
