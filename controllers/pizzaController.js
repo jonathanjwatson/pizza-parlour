de02d46328a0a4bcbed9a6384dbaa98ad4828162
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-10-21 10:57:44 -0400

    pizza controller is complete. .populate is included

--- controllers/pizzaController.js
+++ controllers/pizzaController.js
@@ -4,33 +4,34 @@ const router = express.Router();
 const db = require("../models");
 
 router.get("/api/pizzas", (req, res) => {
-    db.Pizza.find({})
-      .then((foundPizzas) => {
-        res.json(foundPizzas);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: true,
-          data: null,
-          message: "Failed to retrieve pizzas.",
-        });
+  db.Pizza.find({})
+    .populate("ingredients")
+    .then((foundPizzas) => {
+      res.json(foundPizzas);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to retrieve pizzas.",
       });
-  });
-  
-  router.post("/api/pizzas", (req, res) => {
-    db.Pizza.create(req.body)
-      .then((newPizza) => {
-        res.json(newPizza);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: true,
-          data: null,
-          message: "Failed to create new pizza.",
-        });
+    });
+});
+
+router.post("/api/pizzas", (req, res) => {
+  db.Pizza.create(req.body)
+    .then((newPizza) => {
+      res.json(newPizza);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to create new pizza.",
       });
-  });
+    });
+});
 
 module.exports = router;
