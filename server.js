ac3fc4f085ad00f760a347a00edde441db93b6db
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-10-21 10:09:30 -0400

    basic server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
